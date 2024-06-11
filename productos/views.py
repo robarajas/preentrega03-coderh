2e3d4d0f5c0ba4b96b94182e52aebe6dae964fbd
David Barajas Ortega [GDL] <robarajas@compusoluciones.com>
2024-06-10 21:45:51 -0600

    Se integra consulta del módelo de productos para que carge desde base de datos, losproductos existentes en forma de tarjetas, dentro de la vista productos

--- productos/views.py
+++ productos/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from productos.models import Producto
 
 # Create your views here.
 def catalogo(request):
-    return render(request, 'productos/catalogo.html')
+    products_list = Producto.objects.all()
+    return render(request, 'productos/catalogo.html', {'productos': products_list })
