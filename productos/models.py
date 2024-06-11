3c8a5dabc5c1e0fe4f2bc344e470cca9752265fb
David Barajas Ortega [GDL] <robarajas@compusoluciones.com>
2024-06-10 21:05:09 -0600

    Se agrega el primer modelo y su relación con el administrador para poder manipularlo desde admin

--- productos/models.py
+++ productos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Producto(models.Model):
+    sku = models.CharField(max_length=25)
+    nombre = models.CharField(max_length=100)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    descripcion = models.TextField()
+
+    def __str__(self):
+        return self.nombre
