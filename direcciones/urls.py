afd87d1cbe7e5f970717672fc36076735b5a9415
David Barajas Ortega [GDL] <robarajas@compusoluciones.com>
2024-07-06 01:54:22 -0600

    feature: Se implementa edición de dirección

--- direcciones/urls.py
+++ direcciones/urls.py
@@ -3,5 +3,6 @@ from direcciones import views
 
 urlpatterns = [
   path('', views.listado_direcciones, name='listado_direcciones'),
-  path('agregar/', views.agregar_direccion, name='agregar_direccion')
+  path('agregar/', views.agregar_direccion, name='agregar_direccion'),
+  path('editar/<int:pk>/', views.EditarDireccion.as_view(), name='editar_direccion'),
 ]
--- direcciones/views.py
+++ direcciones/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render, redirect
 from direcciones.models import Direccion
 from direcciones.forms import DireccionForm
+from django.contrib.auth.decorators import login_required
+from django.views.generic.edit import UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 
 # Create your views here.
+@login_required
 def listado_direcciones(request):
   direcciones = Direccion.objects.all()
   return render(request, 'direcciones/listado_direcciones.html', {'direcciones': direcciones})
 
+@login_required
 def agregar_direccion(request):
   formulario = DireccionForm();
   if request.method == 'POST':
@@ -17,3 +23,9 @@ def agregar_direccion(request):
       direccion.save()
       return redirect('listado_direcciones')
   return render(request, 'direcciones/agregar_direcciones.html', {'formulario': formulario})
+
+class EditarDireccion(LoginRequiredMixin, UpdateView):
+  model = Direccion
+  fields = ['nombre', 'direccion', 'ciudad', 'codigo_postal']
+  template_name = 'direcciones/editar_direccion.html'
+  success_url = reverse_lazy('listado_direcciones')
