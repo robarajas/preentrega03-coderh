cd47f7c521c22743e9ed454e539ca9a95d66b9ed
David Barajas Ortega [GDL] <robarajas@compusoluciones.com>
2024-07-05 21:26:19 -0600

    Se crea Vista para mostrar detalles ABOUT ME

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Vistas principales
 def main(request):
   return render(request, 'main/inicio.html')
+
+def aboutme(request):
+  return render(request, 'main/aboutme.html')
